9d789958d5b6f27f8ff4766491e1123a29a9d6c6
appsoft-lucio <luciocdesouza@gmail.com>
2025-08-27 19:37:32 -0300

    Nomiei as 3 tabs

--- mobile/UnitPrincipal.pas
+++ mobile/UnitPrincipal.pas
@@ -4,10 +4,14 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.TabControl;
 
 type
   TFormPrincipal = class(TForm)
+    TabControl: TTabControl;
+    TabHome: TTabItem;
+    TabLancamentos: TTabItem;
+    TabUserConfig: TTabItem;
   private
     { Private declarations }
   public
