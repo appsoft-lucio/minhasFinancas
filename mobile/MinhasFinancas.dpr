f7685154c9c034c2de6f9a4eb0b203f24f22b5b7
appsoft-lucio <luciocdesouza@gmail.com>
2025-08-27 19:27:40 -0300

    Adicionei um novo form e 3 tabControl

--- mobile/MinhasFinancas.dpr
+++ mobile/MinhasFinancas.dpr
@@ -3,12 +3,14 @@ program MinhasFinancas;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  UnitLogin in 'UnitLogin.pas' {FormLogin};
+  UnitLogin in 'UnitLogin.pas' {FormLogin},
+  UnitPrincipal in 'UnitPrincipal.pas' {FormPrincipal};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TFormLogin, FormLogin);
+  Application.CreateForm(TFormPrincipal, FormPrincipal);
   Application.Run;
 end.
