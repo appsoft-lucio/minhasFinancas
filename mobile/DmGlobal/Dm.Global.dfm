66564b523cff77f81a5d50ab00d9bb9e0ab03523
appsoft-lucio <luciocdesouza@gmail.com>
2026-02-25 16:23:50 -0300

    Adiciona DataModule global com configuração de serialização e login REST

--- mobile/DmGlobal/Dm.Global.dfm
+++ mobile/DmGlobal/Dm.Global.dfm
@@ -1,4 +1,5 @@
 object DmGlobal: TDmGlobal
+  OnCreate = DataModuleCreate
   Height = 480
   Width = 640
 end
--- mobile/DmGlobal/Dm.Global.pas
+++ mobile/DmGlobal/Dm.Global.pas
@@ -5,10 +5,11 @@ interface
 uses
   System.SysUtils, System.Classes, RESTRequest4D,
   DataSet.Serialize.Adapter.RESTRequest4D,
-  DataSet.Serialize.Config;
+  DataSet.Serialize.Config, System.JSON;
 
 type
   TDmGlobal = class(TDataModule)
+    procedure DataModuleCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -18,14 +19,39 @@ type
 var
   DmGlobal: TDmGlobal;
 
+const
+  BASE_URL = 'http://localhost:3001';
+
 implementation
 
 {%CLASSGROUP 'FMX.Controls.TControl'}
 
 {$R *.dfm}
 
-procedure TDmGlobal.Login(email, senha: string);
+procedure TDmGlobal.DataModuleCreate(Sender: TObject);
 begin
+  TDataSetSerializeConfig.GetInstance.CaseNameDefinition:= cndLower;
+  TDataSetSerializeConfig.GetInstance.Import.DecimalSeparator:= '.';
+end;
+
+procedure TDmGlobal.Login(email, senha: string);
+var
+  resp : IResponse;
+  json : TJsonObject;
+begin
+  try
+    json := TJsonObject.Create;
+    json.AddPair('email', email);
+    json.AddPair('senha', senha)
+
+    resp := TRequest.New.BaseURL(BASE_URL)
+                        .Resource('/usuarios/login')
+                        .AddBody(json.ToString)
+                        .Accept('application/json')
+                        .Post;
+  finally
+    FreeAndNil(json);
+  end;
 
 end;
 
